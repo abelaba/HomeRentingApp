74783625c1ddc33fc190c601e562a110faed0ac2
Ruth Zaid <se.ruth.zaid@gmail.com>
2021-09-09 22:23:50 +0300

    parent class ChatEvent

--- lib/chat/bloc/chat/chat_event.dart
+++ lib/chat/bloc/chat/chat_event.dart
@@ -1,2 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:home_renting_app/chat/models/ChatModel.dart';
+
+abstract class ChatEvent extends Equatable {
+  const ChatEvent();
+
+  @override
+  List<Object> get props => [];
+}
