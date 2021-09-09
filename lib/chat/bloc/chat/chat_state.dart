a8eb5c68ba0d53903e2f1b44bef7d9bcb89b52c1
Ruth Zaid <se.ruth.zaid@gmail.com>
2021-09-09 22:52:12 +0300

    parent class ChatState

--- lib/chat/bloc/chat/chat_state.dart
+++ lib/chat/bloc/chat/chat_state.dart
@@ -1,2 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:home_renting_app/chat/models/ChatModel.dart';
+
+abstract class ChatState extends Equatable {
+  const ChatState();
+
+  @override
+  List<Object> get props => [];
+}
