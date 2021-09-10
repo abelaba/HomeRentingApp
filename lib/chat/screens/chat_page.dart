262b78fcfb8ba2a2cd7a5dd930d24a12981d6f2d
Ruth Zaid <se.ruth.zaid@gmail.com>
2021-09-11 02:12:19 +0300

    chat page - nav to individual chat pages

--- lib/chat/screens/chat_page.dart
+++ lib/chat/screens/chat_page.dart
@@ -15,3 +15,59 @@ class ChatPage extends StatefulWidget {
   @override
   _ChatPageState createState() => _ChatPageState();
 }
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<ChatBloc>(context).add(LoadChats());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget _customCard(ChatModel chatModel) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, IndividualPage.routeName,
+            arguments: ChatArguments(
+                chatId: chatModel.sId!,
+                user1Id: chatModel.user1Id!,
+                user2Id: chatModel.user2Id!,
+                user1Name: chatModel.user1Name!,
+                user2Name: chatModel.user2Name!,
+                messages: chatModel.messages));
+      },
+      child: ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            child: Icon(Icons.person),
+          ),
+          trailing: Text(chatModel.user1Name!),
+          title: Text(chatModel.user2Name!,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          subtitle: Row(
+            children: [
+              Icon(Icons.message_outlined),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                chatModel.messages!.length != 0
+                    ? "${chatModel.messages![chatModel.messages!.length - 1]["message"]}"
+                    : " ",
+                style: TextStyle(fontSize: 13),
+              ),
+            ],
+          )),
+    );
+  }
+
+
+
+
+
+  }
