455b5c99bd162791c7a097a5779ba07fb0da16ff
Ruth Zaid <se.ruth.zaid@gmail.com>
2021-09-11 01:20:35 +0300

    chat page and importing packages

---
+++ lib/chat/screens/chat_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:home_renting_app/chat/bloc/chat/chat_bloc.dart';
+import 'package:home_renting_app/chat/bloc/chat/chat_event.dart';
+import 'package:home_renting_app/chat/bloc/chat/chat_state.dart';
+import 'package:home_renting_app/chat/models/ChatModel.dart';
+import 'package:home_renting_app/chat/screens/messages_page.dart';
+
+import '../../routes.dart';
